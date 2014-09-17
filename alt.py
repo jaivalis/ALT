50f0efadb4a3012e5b487169b6d69c20b1421af3
jaivalis <johnaivalis@gmail.com>
2014-09-18 01:58:23 +0200

    2 probs missing

--- alt.py
+++ alt.py
@@ -2,12 +2,10 @@ import sys
 from collections import Counter
 
 
-def extract_phrases(e_str, f_str, alignments, max_length):
+def extract_phrases(e_tokens, f_tokens, alignments, max_length):
     aligned_phrases = Counter()
     e_phrases = Counter()
     f_phrases = Counter()
-    e_tokens = e_str.strip().split()
-    f_tokens = f_str.strip().split()
 
     for e_start in range(0, len(e_tokens)):
         for e_end in range(e_start, len(e_tokens)):
@@ -29,7 +27,7 @@ def extract_phrases(e_str, f_str, alignments, max_length):
             phrase_alignment = tmp[0]
             e_phrase = tmp[1]
             f_phrase = tmp[2]
-            
+
             aligned_phrases.update(phrase_alignment)
             e_phrases.update(e_phrase)
             f_phrases.update(f_phrase)
@@ -57,9 +55,9 @@ def get_phrase_alignment(e_start, e_end, f_start, f_end, alignments, e_tokens, f
             # add phrase pair (e_start, e_end, f_start, f_end) to returned
             e_phrase = ' '.join(e_tokens[e_start:e_end+1])
             f_phrase = ' '.join(f_tokens[f_s:f_e+1])
-            
+
             pair = e_phrase + '|||' + f_phrase
-            
+
             if pair in aligned_phrases:
                 aligned_phrases[pair] += 1
             else:
@@ -69,12 +67,12 @@ def get_phrase_alignment(e_start, e_end, f_start, f_end, alignments, e_tokens, f
                 e_phrases[e_phrase] += 1
             else:
                 e_phrases[e_phrase] = 1
-                
+
             if f_phrase in f_phrases:
                 f_phrases[f_phrase] += 1
             else:
                 f_phrases[f_phrase] = 1
-                
+
             f_e += 1
             if f_e in f_aligned or f_e == len(f_tokens):  # until f_e aligned
                 break
@@ -84,10 +82,10 @@ def get_phrase_alignment(e_start, e_end, f_start, f_end, alignments, e_tokens, f
     return aligned_phrases, e_phrases, f_phrases
 
 
-def generate_output(phrase_counter, e_phrases, f_phrases, outfile=None):
+def generate_output(phrase_counter, token_pairs, e_phrases, f_phrases, outfile=None):
     """ Generates the formatted output given the phrase counter as asked:
 
-            f ||| e ||| freq(f) freq(e) freq(f, e)
+      f ||| e ||| p(f|e) p(e|f) l(f|e) l(e|f) ||| freq(f) freq(e) freq(f,e)
     FIXME
     """
 
@@ -103,20 +101,21 @@ def generate_output(phrase_counter, e_phrases, f_phrases, outfile=None):
             tok = pair.split('|||')
             e = tok[0]
             f = tok[1]
-            
+
 
             freq_f = f_phrases[f]
             freq_e = e_phrases[e]
             freq_fe = phrase_counter[pair]
             prob_e = float(freq_e) / e_phrases_count
             prob_f = float(freq_f) / f_phrases_count
-            
+
             cond_f_e = float(freq_fe) / freq_f
             cond_e_f = float(freq_fe) / freq_e
-            #l_e_f = 
-            #l_f_e = 
+            l_e_f = -1 # TODO
+            l_f_e = -1 # TODO
 
-            string = "{0} ||| {1} {2:.4f} {3:.4f} {4:} {5:} {6:}".format(f, e, cond_f_e, cond_e_f, freq_f, freq_e, freq_fe)
+            string = "{0} ||| {1} {2:.4f} {3:.4f} {4:.4f} {5:.4f} ||| {6} {7} {8}".\
+                format(f, e, cond_f_e, cond_e_f, l_f_e, l_e_f, freq_f, freq_e, freq_fe)
             out.write(string + '\n')
 
     finally:
@@ -147,7 +146,7 @@ def parse_alignments(alignments_str):
 
 def phrase_extraction(e_path, f_path, aligned_path, max_length):
     # alignments_for_phrases = dict()
-    phrase_pairs = Counter()
+    token_pairs = Counter()
 
     e_phrases = Counter()
     f_phrases = Counter()
@@ -156,10 +155,16 @@ def phrase_extraction(e_path, f_path, aligned_path, max_length):
     with open(e_path, 'r') as e_f, open(f_path, 'r') as f_f, open(aligned_path, 'r') as aligned_f:
         sample_size = 10
         for e_str, f_str, align_str in zip(e_f, f_f, aligned_f):
+            e_tokens = e_str.strip().split()
+            f_tokens = f_str.strip().split()
 
             alignments = parse_alignments(align_str)
+            # Count the token pairs
+            tmp = extract_word_pairs(e_tokens, f_tokens, alignments)
+            token_pairs.update(tmp)
 
-            tmp = extract_phrases(e_str, f_str, alignments, max_length)
+            # Update the various counters
+            tmp = extract_phrases(e_tokens, f_tokens, alignments, max_length)
             phrases_counter = tmp[0]
             e_phrases_counter = tmp[1]
             f_phrases_counter = tmp[2]
@@ -171,7 +176,7 @@ def phrase_extraction(e_path, f_path, aligned_path, max_length):
             if sample_size == 0:
                 break
     print 'Phrases extracted'
-    generate_output(all_phrases_counter, e_phrases, f_phrases, 'file.out')
+    generate_output(all_phrases_counter, token_pairs, e_phrases, f_phrases, 'file.out')
 
 
 def main():
