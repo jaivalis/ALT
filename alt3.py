115f939ebb60dfab6fb3e6f9db0ea2e776ef7ac8
TopSteely <tobias.stahl1@gmx.de>
2014-10-11 18:11:02 +0200

    added swap and discontinous orientations, but something seems to be wrong in the algorithm, it's always monotonic

--- alt3.py
+++ alt3.py
@@ -11,9 +11,9 @@ def find_successors(sentence_tokens, phrases, phrase):
     """
     successors = []
     phrase_tokens = phrase.strip().split()
-    last_word = phrase_tokens[len(phrase_tokens)-1]
+    last_word = phrase_tokens[len(phrase_tokens) - 1]
     last_word_ind = sentence_tokens.index(last_word)
-    if last_word_ind == len(sentence_tokens)-1:  # end of sentence
+    if last_word_ind == len(sentence_tokens) - 1:  # end of sentence
         return successors
     for phr in phrases:
         phr_tokens = phr.strip().split()
@@ -22,7 +22,22 @@ def find_successors(sentence_tokens, phrases, phrase):
     return successors
 
 
-def find_ms(ms, e, f, e_suc, f_suc, e_tokens, f_tokens, string):
+def monotone(f_a, f_b, e_a, e_b):
+    if f_a + 1 == f_b and e_a + 1 == e_b:
+        return True
+
+
+def swap(f_a, f_b, e_a, e_b):
+    if f_a + 1 == f_b and e_a == e_b + 1:
+        return True
+
+
+def discontinuous(f_a, f_b, e_a, e_b):
+    if not monotone(f_a, f_b, e_a, e_b) and not swap(f_a, f_b, e_a, e_b):
+        return True
+
+
+def find_orientations(ms, e, f, e_suc, f_suc, e_tokens, f_tokens):
     """
     finds phrases with monotonic prientation with the following characteristic:
     there exists a (f',e')-pair, for which right(e') = left(e)-1 AND right(f') = left(f)-1
@@ -35,45 +50,47 @@ def find_ms(ms, e, f, e_suc, f_suc, e_tokens, f_tokens, string):
     :param string: left-right or right-left
     :return:
     """
-    if f not in ms:
-        ms[f] = {}
     for e_s, f_s in zip(e_suc, f_suc):
-        print e, e_s, f, f_s
         if len(e_suc) == 1 and len(f_suc) == 1:
             # find last words of current phrases and first words of successors
             last_f = f.strip().split()[-1]
             last_e = e.strip().split()[-1]
             first_e_s = e_s.strip().split()[0]
             first_f_s = f_s.strip().split()[0]
-            if f_tokens.index(last_f) == e_tokens.index(first_e_s) - 1 and e_tokens.index(last_e) == f_tokens.index(first_f_s) - 1:
-                if e in ms[f]:
-                    ms[f][e] += 1
-                else:
-                    ms[f][e] = 1
-        #else:  # more than one successor in one of the languages. Problem: different amounts of successors
-            # for ee, ff in zip(e_s, f_s):
-            #     if f_tokens.index(f) == e_tokens.index(ee) - 1 and e_tokens.index(e) == f_tokens.index(ff) - 1:
-            #         if e in ms[f]:
-            #             ms[f[e]] += 1
-            #         else:
-            #             ms[f[e]] = 1
+            # find indexes of those first and last words
+            f_b = f_tokens.index(first_f_s)
+            f_a = f_tokens.index(last_f)
+            e_b = e_tokens.index(first_e_s)
+            e_a = e_tokens.index(last_e)
+            print f_a, f_b, '||', e_a, e_b
+            if monotone(f_a, f_b, e_a, e_b):
+                print "mono"
+                if f not in ms:
+                    ms[f] = {}
+                    if e in ms[f]:
+                        ms[f][e] += 1
+                    else:
+                        ms[f][e] = 1
+            if swap(f_a, f_b, e_a, e_b):
+                print "swap"
+                pass
+            if discontinuous(f_a, f_b, e_a, e_b):
+                print "dis"
+                pass
+            raw_input("BA")
+            #if
+
+                    # else:  # more than one successor in one of the languages. Problem: different amounts of successors
+                    # for ee, ff in zip(e_s, f_s):
+                    #     if f_tokens.index(f) == e_tokens.index(ee) - 1 and e_tokens.index(e) == f_tokens.index(ff) - 1:
+                    #         if e in ms[f]:
+                    #             ms[f[e]] += 1
+                    #         else:
+                    #             ms[f[e]] = 1
     return ms
 
 
-def find_ss(e, f, e_phrases, f_phrases, e_str, f_str, string):
-    return 0
-
-
-def find_dls(e, f, e_phrases, f_phrases, e_str, f_str, string):
-    return 0
-
-
-def find_drs(e, f, e_phrases, f_phrases, e_str, f_str, string):
-    return 0
-
-
 def orientation_extraction(e_path, f_path, aligned_path, max_length):
-
     m_counter_rl = Counter()
     dl_counter_rl = Counter()
     dr_counter_rl = Counter()
@@ -100,11 +117,10 @@ def orientation_extraction(e_path, f_path, aligned_path, max_length):
             for e, f in zip(e_phrases, f_phrases):
                 e_suc = find_successors(e_tokens, e_phrases, e)
                 f_suc = find_successors(f_tokens, f_phrases, f)
-                ms = find_ms(ms, e, f, e_suc, f_suc, e_tokens, f_tokens, "rl")
+                ms = find_orientations(ms, e, f, e_suc, f_suc, e_tokens, f_tokens)
 
             print ms
             raw_input("Bla:")
-
 
             sample_size -= 1
             if sample_size == 0:
